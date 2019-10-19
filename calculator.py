fa805648cc0b1f0eb6dc35708bb1864e576d44be
Your Name <you@example.com>
2019-10-19 12:43:58 -0700

    Check input for validity or quit

--- calculator.py
+++ calculator.py
@@ -1,17 +1,24 @@
 """A prefix-notation calculator.
 
-Using the arithmetic.py file from Calculator Part 1, create the
-calculator program yourself in this file.
+Uses functions in arithmetic.py
+Limited to a pre-defined number of integer inputs.
 """
 
 from arithmetic import *
 
 
-# Your code goes here
 print('Welcome to the prefix calculator.')
 print('Please enter a mathematical function and integers to be evaluated.')
-print(f'Available functions:\n+ : sum two integers\n- : subtract two integers\n* : multiply two integers\n/ : divide the first integer by the second integer\nsq : square two integers\ncube : cube two integers\npow : raise one integer to the power of the second integer\nmod : return the remainder of the first integer divided by the second')
+print(f'Available functions:\n+ : sum two integers\n- : subtract two integers\n* : multiply two integers\n/ : divide the first integer by the second integer\nsq : square two integers\ncube : cube two integers\npow : raise one integer to the power of the second integer\nmod : return the remainder of the first integer divided by the second\nq : quit')
 
-to_math = input('Input an operation and two integers > ')
+while True:
 
-print(to_math)
+    to_math = input('Input an operation and two integers > ')
+    equation = to_math.split(" ")
+
+    if equation[0] == 'q':
+        exit()
+    elif len(equation) >= 3 and len(equation) <= 4:
+        print(equation)
+    else:
+        print('Please review the instructions and enter correct input.')
