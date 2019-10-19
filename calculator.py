df18e130ace593cd93a9fb7190f56a23e71a081d
Your Name <you@example.com>
2019-10-19 13:00:18 -0700

    Add squaring conditional

--- calculator.py
+++ calculator.py
@@ -7,7 +7,8 @@ Limited to a pre-defined number of integer inputs.
 from arithmetic import *
 
 
-print('Welcome to the prefix calculator.')
+print('* * * Welcome to the prefix calculator * * *')
+print('This calculator takes integer input and returns floats.')
 print('Please enter a mathematical function and integers to be evaluated.')
 print(f'Available functions:\n+ : sum two integers\n- : subtract two integers\n* : multiply two integers\n/ : divide the first integer by the second integer\nsq : square two integers\ncube : cube two integers\npow : raise one integer to the power of the second integer\nmod : return the remainder of the first integer divided by the second\nq : quit')
 
@@ -16,8 +17,18 @@ while True:
     to_math = input('Input an operation and two integers > ')
     equation = to_math.split(" ")
 
+    def fail():
+        print('Please review the instructions and enter correct input.')
+
     if equation[0] == 'q':
         exit()
+    elif len(equation) == 2:
+        op = equation[0]
+        num1 = int(equation[1])
+        if op == 'sq':
+            print(square(num1))
+        else:
+            fail()
     elif len(equation) == 3:
         op = equation[0]
         num1 = int(equation[1])
@@ -30,5 +41,7 @@ while True:
             print(multiply(num1, num2))
         elif op == '/':
             print(divide(num1, num2))
+        elif op == 'sq':
+            print(square(num1))
     else:
-        print('Please review the instructions and enter correct input.')
+        fail()
