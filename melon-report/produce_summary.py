4bad29dbb5b8ab64a9ba6c819d5d0f71474d4c5c
Your Name <you@example.com>
2019-10-19 17:39:01 -0700

    Use function for all three days

--- melon-report/produce_summary.py
+++ melon-report/produce_summary.py
@@ -19,7 +19,6 @@ def delivery_info(delivery_file):
     daily = [daily_count, daily_amount]
     return daily
 
-
 print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
 daily_results = delivery_info(the_file)
@@ -28,31 +27,15 @@ print('')
 print(f'Day 1\'s Total: {daily_results[0]} melons for a total of ${daily_results[1]}.')
 the_file.close()
 
-"""
 print('')
 print('')
 
 print("Day 2")
 the_file = open("um-deliveries-20140520.txt")
-daily_count = 0
-daily_amount = 0
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
-
-    melon = words[0]
-    count = int(words[1])
-    amount = float(words[2])
-
-    daily_count = daily_count + count
-    daily_amount = daily_amount + amount
-
-    print("     Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+daily_results = delivery_info(the_file)
 
 print('')
-print(f'Day 2\'s Total: {daily_count} melons for a total of ${daily_amount}.')
+print(f'Day 2\'s Total: {daily_results[0]} melons for a total of ${daily_results[1]}.')
 the_file.close()
 
 print('')
@@ -60,23 +43,11 @@ print('')
 
 print("Day 3")
 the_file = open("um-deliveries-20140521.txt")
-daily_count = 0
-daily_amount = 0
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+daily_results = delivery_info(the_file)
 
-    melon = words[0]
-    count = int(words[1])
-    amount = float(words[2])
-
-    daily_count = daily_count + count
-    daily_amount = daily_amount + amount
-
-    print("     Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
+print('')
+print(f'Day 3\'s Total: {daily_results[0]} melons for a total of ${daily_results[1]}.')
 the_file.close()
 
 print('')
-print(f'Day 3\'s Total: {daily_count} melons for a total of ${daily_amount}.')
-the_file.close()"""
+print('')
