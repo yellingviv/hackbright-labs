805ba129d8a233a869927e58d64c82365136e57b
Your Name <you@example.com>
2019-10-19 17:42:24 -0700

    Comment yr code, girl

--- melon-report/produce_summary.py
+++ melon-report/produce_summary.py
@@ -1,8 +1,12 @@
 """ This prints out the reports of the daily melon deliveries for Ubermelon"""
 
 def delivery_info(delivery_file):
+    # iterate through the delivery file to establish the total sales for the day
+
     daily_count = 0
     daily_amount = 0
+    # start with the count and price of melons sold at zero
+
     for line in the_file:
         line = line.rstrip()
         words = line.split('|')
@@ -13,19 +17,27 @@ def delivery_info(delivery_file):
 
         daily_count = daily_count + count
         daily_amount = daily_amount + amount
+        # add the count of melons and price of melons for this line to the daily total
 
         print("     Delivered {} {}s for total of ${}".format(count, melon, amount))
+        # print only the quantity of melons and total cost for that specific melon varietal
 
     daily = [daily_count, daily_amount]
     return daily
+    # return back the daily total quantity of melons sold for total price
 
 print("Day 1")
 the_file = open("um-deliveries-20140519.txt")
+# select the file for that day - should be possible to do this more cleanly
+
 daily_results = delivery_info(the_file)
+# pass the daily delivery info file to the evaluation function
 
 print('')
 print(f'Day 1\'s Total: {daily_results[0]} melons for a total of ${daily_results[1]}.')
+# print the total sold and earned for the day
 the_file.close()
+# close the file to move to the next day's file
 
 print('')
 print('')
