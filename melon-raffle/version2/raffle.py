9d94e6192dbfa52cc5d418774fd509a41537f8ca
Vivienne <viv@yellingviv.com>
2019-11-07 15:41:58 -0800

    add refactored code to keep customer data separate in version2 folder

--- melon-raffle/version2/raffle.py
+++ melon-raffle/version2/raffle.py
@@ -4,3 +4,25 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+
+from random import choice
+import customers
+
+def pick_winner(customer_list):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = choice(customer_list)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+        ))
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customer_list = customers.get_customers_from_file("customers.txt")
+    pick_winner(customer_list)
+
+run_raffle()
